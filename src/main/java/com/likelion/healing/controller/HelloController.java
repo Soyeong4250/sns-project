c99840af6c2d6fee12708b757b01e09430784998
이소영 <th42500@naver.com>
2022-12-20 13:07:07 +0900

    [Feat] HelloController - Swagger 적용

--- src/main/java/com/likelion/healing/controller/HelloController.java
+++ src/main/java/com/likelion/healing/controller/HelloController.java
@@ -1,15 +1,20 @@
 package com.likelion.healing.controller;
 
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@Slf4j
+@RequestMapping("/api/v1/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @ApiOperation(value = "hello 출력", notes = "Get Method")
+    @GetMapping()
     public String printHello() {
+        log.debug("printHello() 실행");
         return "hello";
     }
 }
