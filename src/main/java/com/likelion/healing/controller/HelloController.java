568f815361699afe77f79845f9f61e150cb13b80
이소영 <th42500@naver.com>
2022-12-20 14:30:02 +0900

    [Feat] Swagger Config 추가 및 Swagger 명세 설명 수정

--- src/main/java/com/likelion/healing/controller/HelloController.java
+++ src/main/java/com/likelion/healing/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.likelion.healing.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@Api(tags = {"00. HelloController"})
 @RequestMapping("/api/v1/hello")
 public class HelloController {
 
-    @ApiOperation(value = "hello 출력", notes = "Get Method")
+    @ApiOperation(value = "hello 출력", notes = "Get Method를 이용하여 hello 문자열을 출력합니다.")
     @GetMapping()
     public String printHello() {
         log.debug("printHello() 실행");
