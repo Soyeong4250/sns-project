82b04327503acaccb7bbc64e4758a30f84281ccd
이소영 <th42500@naver.com>
2022-12-20 11:33:41 +0900

    [Build] 프로젝트 생성 및 application 설정

---
+++ src/main/java/com/likelion/healing/HealingSnsApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.healing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/healing/HealingSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.healing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
