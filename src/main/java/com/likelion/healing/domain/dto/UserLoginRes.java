ec1800db04820d179e9b815819a1dff16b9a4138
이소영 <th42500@naver.com>
2022-12-21 17:07:50 +0900

    [Feat] UserLoginRes - 로그인 응답 dto

---
+++ src/main/java/com/likelion/healing/domain/dto/UserLoginRes.java
@@ -0,0 +1,12 @@
+package com.likelion.healing.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserLoginRes {
+
+    private String jwt;
+
+}
