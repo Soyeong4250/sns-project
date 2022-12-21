139773dd9c2e00f48a7d4900365af29dc5ce7cf0
이소영 <th42500@naver.com>
2022-12-21 14:46:41 +0900

    [Test] 회원가입 테스트
    1. 회원가입 성공
    2. 회원가입 실패 - userName 중복인 경우

---
+++ src/test/java/com/likelion/healing/controller/UserControllerTest.java
@@ -0,0 +1,73 @@
+package com.likelion.healing.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.healing.domain.dto.UserJoinReq;
+import com.likelion.healing.domain.dto.UserJoinRes;
+import com.likelion.healing.exception.ErrorCode;
+import com.likelion.healing.exception.HealingSnsAppException;
+import com.likelion.healing.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void successfulJoin() throws Exception {
+        UserJoinReq req = UserJoinReq.builder()
+                .userName("Soyeong")
+                .password("12345")
+                .build();
+
+        given(userService.join(any(UserJoinReq.class))).willReturn(new UserJoinRes(1, "Soyeong"));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(req)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+                .andExpect(jsonPath("$.result.userId").value(1))
+                .andExpect(jsonPath("$.result.userName").value("Soyeong"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - userName 중복인 경우")
+    void failedJoin() throws Exception {
+        UserJoinReq req = UserJoinReq.builder()
+                .userName("Soyeong")
+                .password("12345")
+                .build();
+
+        given(userService.join(any(UserJoinReq.class))).willThrow(new HealingSnsAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s은(는) 이미 있습니다.", req.getUserName())));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(req)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+}
