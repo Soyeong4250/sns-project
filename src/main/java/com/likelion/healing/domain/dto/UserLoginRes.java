48ec65142a4f7bf5ba8d9f68a6ddee9a7f88b7b8
이소영 <th42500@naver.com>
2022-12-21 18:18:38 +0900

    [Refactor] UserLoginRes - builder 추가
    1. @NoArgsConstructor 선언
    2. @Builder 선언

--- src/main/java/com/likelion/healing/domain/dto/UserLoginRes.java
+++ src/main/java/com/likelion/healing/domain/dto/UserLoginRes.java
@@ -1,9 +1,13 @@
 package com.likelion.healing.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Getter
 public class UserLoginRes {
 
