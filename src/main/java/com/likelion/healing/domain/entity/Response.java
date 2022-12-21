134601b06211c76de4fd38554e826e591579dd13
이소영 <th42500@naver.com>
2022-12-21 14:44:47 +0900

    [Feat] Response 객체

---
+++ src/main/java/com/likelion/healing/domain/entity/Response.java
@@ -0,0 +1,20 @@
+package com.likelion.healing.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    public static <T> Response<T> error(T result) {
+        return new Response<>("ERROR", result);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS", result);
+    }
+
+}
