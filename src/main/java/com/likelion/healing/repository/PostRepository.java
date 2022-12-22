d697b0d763b3cc577e1955765414e7dfbbecd286
이소영 <th42500@naver.com>
2022-12-22 15:16:29 +0900

    [Feat] PostRepository - 포스트 목록 날짜 내림차순 조회

--- src/main/java/com/likelion/healing/repository/PostRepository.java
+++ src/main/java/com/likelion/healing/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.likelion.healing.repository;
 
 import com.likelion.healing.domain.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
 }
