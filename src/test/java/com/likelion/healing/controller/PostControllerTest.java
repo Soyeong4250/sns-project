de0240c5c555dda201496b6fa0419adc845cbf5b
이소영 <th42500@naver.com>
2022-12-22 10:45:11 +0900

    [Test] PostContorollerTest - 포스트 등록 잘못된 Token일 경우 테스트
    Token이 'Bearer '로 시작하지 않는 경우 테스트

--- src/test/java/com/likelion/healing/controller/PostControllerTest.java
+++ src/test/java/com/likelion/healing/controller/PostControllerTest.java
@@ -3,6 +3,8 @@ package com.likelion.healing.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.likelion.healing.domain.dto.PostAddReq;
 import com.likelion.healing.domain.dto.PostAddRes;
+import com.likelion.healing.exception.ErrorCode;
+import com.likelion.healing.exception.HealingSnsAppException;
 import com.likelion.healing.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,7 +44,7 @@ class PostControllerTest {
                 .body("body1")
                 .build();
 
-        given(postService.addPost(any(PostAddReq.class), any(String.class))).willReturn(new PostAddRes("포스트 등록 완료", 1));
+        given(postService.addPost(any(PostAddReq.class), "Bearer " + any(String.class))).willReturn(new PostAddRes("포스트 등록 완료", 1));
 
         mockMvc.perform(post("/api/v1/posts")
                 .with(csrf())
@@ -54,4 +56,26 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.result.message").value("포스트 등록 완료"))
                 .andExpect(jsonPath("$.result.postId").value(1));
     }
+
+    @Test
+    @WithMockUser
+    @DisplayName("포스트 작성 실패 - JWT를 Bearer Token으로 보내지 않은 경우")
+    void notStartsWithBearer() throws Exception {
+        PostAddReq req = PostAddReq.builder()
+                .title("title1")
+                .body("body1")
+                .build();
+
+        given(postService.addPost(any(PostAddReq.class), "Bearerrrr " + any(String.class))).willThrow(new HealingSnsAppException(ErrorCode.INVALID_TOKEN, "잘못된 토큰입니다."));
+
+        mockMvc.perform(post("/api/v1/posts")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(req)))
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.result.errorCode").value("INVALID_TOKEN"))
+                .andExpect(jsonPath("$.result.message").value("잘못된 토큰입니다."));
+    }
+
 }
