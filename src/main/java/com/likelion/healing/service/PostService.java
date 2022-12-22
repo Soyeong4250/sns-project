95683624db40f2c6315232a72c73ce9c39abba08
이소영 <th42500@naver.com>
2022-12-22 09:24:48 +0900

    [Feat] PostService - 포스트 등록 로직

--- src/main/java/com/likelion/healing/service/PostService.java
+++ src/main/java/com/likelion/healing/service/PostService.java
@@ -1,6 +1,9 @@
 package com.likelion.healing.service;
 
 import com.likelion.healing.domain.dto.PostAddReq;
+import com.likelion.healing.domain.dto.PostAddRes;
+import com.likelion.healing.domain.entity.Post;
+import com.likelion.healing.domain.entity.User;
 import com.likelion.healing.exception.ErrorCode;
 import com.likelion.healing.exception.HealingSnsAppException;
 import com.likelion.healing.repository.PostRepository;
@@ -17,8 +20,19 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
-    public void addPost(PostAddReq postAddReq, String userName) {
-        userRepository.findByUserName(userName)
+    public PostAddRes addPost(PostAddReq postAddReq, String userName) {
+        User user = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new HealingSnsAppException(ErrorCode.NOT_FOUND, String.format("%s은(는) 없는 회원입니다.", userName)));
+
+        Post post = Post.builder()
+                .title(postAddReq.getTitle())
+                .body(postAddReq.getBody())
+                .user(user)
+                .build();
+        Post savedPost = postRepository.save(post);
+        return PostAddRes.builder()
+                .postId(savedPost.getId())
+                .message("포스트 등록 완료")
+                .build();
     }
 }
