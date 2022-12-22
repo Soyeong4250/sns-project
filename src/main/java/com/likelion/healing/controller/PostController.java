253719a8d1e4cf44f4c7ea665b0b7ed460c465c8
이소영 <th42500@naver.com>
2022-12-22 12:41:40 +0900

    [Feat] PostController - 포스트 목록 조회 기본코드 추가

--- src/main/java/com/likelion/healing/controller/PostController.java
+++ src/main/java/com/likelion/healing/controller/PostController.java
@@ -2,15 +2,16 @@ package com.likelion.healing.controller;
 
 import com.likelion.healing.domain.dto.PostAddReq;
 import com.likelion.healing.domain.dto.PostAddRes;
+import com.likelion.healing.domain.dto.PostViewRes;
 import com.likelion.healing.domain.entity.Response;
 import com.likelion.healing.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -27,5 +28,12 @@ public class PostController {
         log.info("userName : {}", userName);
         PostAddRes postAddRes = postService.addPost(postAddReq, userName);
         return Response.success(new PostAddRes(postAddRes.getMessage(), postAddRes.getPostId()));
+    }
+
+    @GetMapping()
+    public Response<List<PostViewRes>> getPostList(Pageable pageable) {
+        log.debug("getPostList() 실행");
+
+        return Response.success(postService.getPostList(pageable));
     }
 }
