39d350a648cbe8c06a55d72f6b59224e45fd0ed9
이소영 <th42500@naver.com>
2022-12-23 11:45:40 +0900

    [Test] PostServiceTest - TestFixture 적용

--- src/test/java/com/likelion/healing/service/PostServiceTest.java
+++ src/test/java/com/likelion/healing/service/PostServiceTest.java
@@ -5,9 +5,10 @@ import com.likelion.healing.domain.dto.PostRes;
 import com.likelion.healing.domain.dto.PostViewRes;
 import com.likelion.healing.domain.entity.Post;
 import com.likelion.healing.domain.entity.User;
-import com.likelion.healing.domain.entity.UserRole;
 import com.likelion.healing.exception.ErrorCode;
 import com.likelion.healing.exception.HealingSnsAppException;
+import com.likelion.healing.fixture.PostEntityFixture;
+import com.likelion.healing.fixture.TestInfoFixture;
 import com.likelion.healing.repository.PostRepository;
 import com.likelion.healing.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -15,73 +16,52 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.sql.Timestamp;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class PostServiceTest {
 
-    private PostRepository postRepository = Mockito.mock(PostRepository.class);
-    private UserRepository userRepository = Mockito.mock(UserRepository.class);
+    private PostRepository postRepository = mock(PostRepository.class);
+    private UserRepository userRepository = mock(UserRepository.class);
     private PostService postService = new PostService(postRepository, userRepository);
 
     @Test
     @DisplayName("포스트 등록 성공")
     void successfulAddPost() {
-        User givenUser = User.builder()
-                .userName("Soyeong")
-                .role(UserRole.USER)
-                .build();
-        Mockito.when(userRepository.findByUserName("Soyeong"))
-                .thenReturn(Optional.of(givenUser));
+        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
 
-        Post givenPost = Post.builder()
-                .id(1)
-                .title("title1")
-                .body("body1")
-                .user(givenUser)
-                .build();
+        User mockUser = mock(User.class);
+        Post mockPost = mock(Post.class);
+
+        Mockito.when(userRepository.findByUserName(fixture.getUserName()))
+                .thenReturn(Optional.of(mockUser));
+
         Mockito.when(postRepository.save(any(Post.class)))
-                .thenReturn(givenPost);
+                .thenReturn(mockPost);
 
-
-        PostRes postRes = postService.addPost(
-                new PostAddReq("title1", "body1"), givenUser.getUserName());
-
-        Assertions.assertEquals(1, postRes.getPostId());
-        Assertions.assertEquals("포스트 등록 완료", postRes.getMessage());
-
-        verify(postRepository).save(any());
+        Assertions.assertDoesNotThrow(() -> postService.addPost(new PostAddReq(fixture.getTitle(), fixture.getBody()), fixture.getUserName()));
     }
 
     @Test
     @DisplayName("포스트 등록 실패 - 회원이 존재하지 않을 때")
     void notFoundUser() {
-        User givenUser = User.builder()
-                .userName("Soyeong")
-                .role(UserRole.USER)
-                .build();
-        Mockito.when(userRepository.findByUserName("Soyeong"))
-                .thenThrow(new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 없는 회원입니다.", givenUser.getUserName())));
+        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
 
-        Post givenPost = Post.builder()
-                .id(1)
-                .title("title1")
-                .body("body1")
-                .user(givenUser)
-                .build();
+        User givenUser = mock(User.class);
+        Mockito.when(userRepository.findByUserName(fixture.getUserName()))
+                .thenThrow(new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 없는 회원입니다.", fixture.getUserName())));
+
+        Post mockPost = mock(Post.class);
         Mockito.when(postRepository.save(any(Post.class)))
-                .thenReturn(givenPost);
+                .thenReturn(mockPost);
 
         try {
             PostRes postRes = postService.addPost(
                     new PostAddReq("title1", "body1"), givenUser.getUserName());
         } catch (HealingSnsAppException e) {
             Assertions.assertEquals(ErrorCode.USERNAME_NOT_FOUND, e.getErrorCode());
-            Assertions.assertEquals("Soyeong은(는) 없는 회원입니다.", e.getMessage());
         }
 
         verify(postRepository, never()).save(any());
@@ -90,36 +70,27 @@ class PostServiceTest {
     @Test
     @DisplayName("포스트 단건 조회 성공")
     void successfulGetPostById() {
+        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
         User givenUser = User.builder()
-                .userName("Soyeong")
-                .role(UserRole.USER)
-                .build();
-        Mockito.when(userRepository.findByUserName("Soyeong"))
+                            .userName(fixture.getUserName())
+                            .build();
+        Mockito.when(userRepository.findByUserName(fixture.getUserName()))
                 .thenReturn(Optional.of(givenUser));
 
-        Post givenPost = Post.builder()
-                .id(1)
-                .title("title1")
-                .body("body1")
-                .user(givenUser)
-                .build();
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-        givenPost.setCreatedAt(now);
-        givenPost.setUpdatedAt(now);
+        Post givenPost = PostEntityFixture.get(fixture.getUserName(), fixture.getPassword());
+
         Mockito.when(postRepository.findById(any(Integer.class)))
                 .thenReturn(Optional.of(givenPost));
 
+        PostViewRes postViewRes = postService.getPostById(fixture.getPostId());
 
-        PostViewRes postViewRes = postService.getPostById(1);
-
-        Assertions.assertEquals(1, postViewRes.getId());
-        Assertions.assertEquals("title1", postViewRes.getTitle());
-        Assertions.assertEquals("body1", postViewRes.getBody());
-        Assertions.assertEquals("Soyeong", postViewRes.getUserName());
+        Assertions.assertEquals(fixture.getPostId(), postViewRes.getId());
+        Assertions.assertEquals(fixture.getTitle(), postViewRes.getTitle());
+        Assertions.assertEquals(fixture.getBody(), postViewRes.getBody());
+        Assertions.assertEquals(fixture.getUserName(), postViewRes.getUserName());
         Assertions.assertNotNull(postViewRes.getCreatedAt());
         Assertions.assertNotNull(postViewRes.getLastModifiedAt());
 
         verify(postRepository).findById(any());
     }
-
 }
