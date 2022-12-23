b120da55e1075096b20520097f02cea71e5120e0
이소영 <th42500@naver.com>
2022-12-23 16:42:23 +0900

    [Feat] ErrorCode, ExceptionManager에 데이터베이스 예외 추가

--- src/main/java/com/likelion/healing/exception/ExceptionManager.java
+++ src/main/java/com/likelion/healing/exception/ExceptionManager.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @RestControllerAdvice
 public class ExceptionManager {
 
@@ -13,6 +15,13 @@ public class ExceptionManager {
     public ResponseEntity<?> healingSnsAppExceptionHandler(HealingSnsAppException e) {
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(Response.error(new ErrorResult(e.getErrorCode(), e.getMessage())));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> databaseExceptionHandler(SQLException e) {
+        ErrorResult errorResult = new ErrorResult(ErrorCode.DATABASE_ERROR, e.getMessage());
+        return ResponseEntity.status(errorResult.getErrorCode().getStatus())
+                .body(Response.error(errorResult));
     }
 
 }
