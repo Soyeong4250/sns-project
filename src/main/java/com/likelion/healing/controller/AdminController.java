55770944e9912f796cae4535829f32ba8780360f
이소영 <th42500@naver.com>
2022-12-24 23:53:40 +0900

    [Refactor] AdminController - 모든 회원 리스트 조회 Pageable 적용

--- src/main/java/com/likelion/healing/controller/AdminController.java
+++ src/main/java/com/likelion/healing/controller/AdminController.java
@@ -3,9 +3,14 @@ package com.likelion.healing.controller;
 import com.likelion.healing.domain.dto.AdminViewRes;
 import com.likelion.healing.domain.entity.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
 
 @RestController
 @Slf4j
@@ -13,9 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     @GetMapping("/users")
-    public Response<AdminViewRes> getAllUsers() {
+    public Response<Page<AdminViewRes>> getAllUsers(@PageableDefault(size = 20, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
         log.debug("getAllUsers() 실행");
-        
+
         return Response.success(null);
     }
 }
