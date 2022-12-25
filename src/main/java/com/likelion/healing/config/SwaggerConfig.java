749c280119c2009e4d349c61f9b95732239db019
이소영 <th42500@naver.com>
2022-12-25 23:56:54 +0900

    [Refactor] SwaggerConfig - Authorization 적용,  Contact 정보 추가

--- src/main/java/com/likelion/healing/config/SwaggerConfig.java
+++ src/main/java/com/likelion/healing/config/SwaggerConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +20,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.likelion.healing.controller"))
                 .paths(PathSelectors.any())
@@ -27,6 +33,24 @@ public class SwaggerConfig {
                 .title("Likelion SNS API with Swagger")
                 .description("Swagger를 이용한 SNS Project API 명세")
                 .version("1.0.0")
+                .contact(new Contact("Soyeong Lee", "https://gitlab.com/th42500/finalproject_leesoyeong", "th42500@naver.com"))
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 }
