83d6495019c7699e1f748a13824d9fba820bbb33
이소영 <th42500@naver.com>
2022-12-27 17:03:23 +0900

    [Refactor] HealingSnsAppException - 필요없는 공백 제거

--- src/main/java/com/likelion/healing/exception/HealingSnsAppException.java
+++ src/main/java/com/likelion/healing/exception/HealingSnsAppException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class HealingSnsAppException extends  RuntimeException{
+public class HealingSnsAppException extends RuntimeException{
 
     private ErrorCode errorCode;
     private String message;
