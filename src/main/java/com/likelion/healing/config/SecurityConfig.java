3047a47808d02ac60862d853e34215e50fd31e0d
이소영 <th42500@naver.com>
2022-12-27 17:00:31 +0900

    [Refactor] SecurityConfig - EnableGlobalMethodSecurity(securedEnabled = true) 적용

--- src/main/java/com/likelion/healing/config/SecurityConfig.java
+++ src/main/java/com/likelion/healing/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -33,7 +34,7 @@ public class SecurityConfig {
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                 .antMatchers("**exception**").permitAll()
-                .antMatchers("/api/v1/users/{userId}/role/change").hasRole("ADMIN")
+//                .antMatchers("/api/v1/users/{userId}/role/change").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
