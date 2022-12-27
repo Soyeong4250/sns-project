422b71b6111c29c22858df0e71acd439744207a0
이소영 <th42500@naver.com>
2022-12-27 16:00:34 +0900

    [Refactor] UserRole - 권한이 `ROLE_`로 시작할 수 있도록 수정
    Security에서 권장하는 `ROLE_`로 시작하는 권한명으로 수정

--- src/main/java/com/likelion/healing/domain/entity/UserRole.java
+++ src/main/java/com/likelion/healing/domain/entity/UserRole.java
@@ -1,13 +1,20 @@
 package com.likelion.healing.domain.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public enum UserRole {
 
-    ADMIN("ADMIN"),
-    USER("USER")
+    ADMIN(Authority.ADMIN),
+    USER(Authority.USER)
     ;
 
-    private String name;
+    private final String authority;
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
 }
