b16c60154316c0547751231a989927ab9dba0ee9
이소영 <th42500@naver.com>
2022-12-29 09:29:03 +0900

    [Rename] 사용자 권한 변경 요청 dto명 변경
    UpdateUserRoleReq 👉 UserRoleUpdateReq

--- src/main/java/com/likelion/healing/domain/dto/UserRoleUpdateReq.java
+++ src/main/java/com/likelion/healing/domain/dto/UserRoleUpdateReq.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 @Schema(description = "권한 변경 요청 dto")
-public class UpdateUserRoleReq {
+public class UserRoleUpdateReq {
 
     @Schema(description = "변경하고 싶은 권한")
     private UserRole role;
