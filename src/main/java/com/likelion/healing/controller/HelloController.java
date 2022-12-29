46b920332613b1bcc87d5915a587e898732a4722
이소영 <th42500@naver.com>
2022-12-29 10:06:12 +0900

    [Feat] HelloController - 모든 자릿수의 합 출력 기능 추가

--- src/main/java/com/likelion/healing/controller/HelloController.java
+++ src/main/java/com/likelion/healing/controller/HelloController.java
@@ -1,22 +1,32 @@
 package com.likelion.healing.controller;
 
+import com.likelion.healing.service.AlgorithmService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @Api(tags = {"00. HelloController"})
 @RequestMapping("/api/v1/hello")
 public class HelloController {
 
-    @ApiOperation(value = "cicd test 출력", notes = "Get Method를 이용하여 cicd test 문자열을 출력합니다.")
+    private final AlgorithmService algorithmService;
+
+    @ApiOperation(value = "cicd test 출력", notes = "GetMapping를 이용하여 cicd test 문자열을 출력합니다.")
     @GetMapping()
     public String printHello() {
         log.debug("printHello() 실행");
         return "이소영";
     }
+
+    @ApiOperation(value = "모든 자릿수의 합 출력해보기", notes = "PostMapping을 이용하여 sumOfDigit 결과를 출력합니다.")
+    @PostMapping("/{num}")
+    public Integer sumOfDigit(@PathVariable  Integer num) {
+        return algorithmService.sumOfDigit(num);
+    }
+
 }
