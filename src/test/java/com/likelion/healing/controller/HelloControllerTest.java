28689016c73b57e462abbefd5f4135cd95349d4f
이소영 <th42500@naver.com>
2022-12-29 10:14:27 +0900

    [Test] HelloCotrollerTest - sumOfDigit() 잘못된 테스트 제거

--- src/test/java/com/likelion/healing/controller/HelloControllerTest.java
+++ src/test/java/com/likelion/healing/controller/HelloControllerTest.java
@@ -1,8 +1,6 @@
 package com.likelion.healing.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.likelion.healing.exception.ErrorCode;
-import com.likelion.healing.exception.HealingSnsAppException;
 import com.likelion.healing.service.AlgorithmService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +15,6 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloController.class)
@@ -44,24 +41,6 @@ class HelloControllerTest {
         mockMvc.perform(post(String.format("/api/v1/hello/%d", num))
                 .with(csrf()))
                 .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    @WithMockUser
-    @DisplayName("sumOfDigit 실패 - 0이하의 수인 경우")
-    void failedSumOfDigit() throws Exception {
-        Integer num = 0;
-        Integer sum = 24;
-
-        given(algorithmService.sumOfDigit(any(Integer.class))).willThrow(new HealingSnsAppException(ErrorCode.BY_ZERO_ERROR, "0보다 큰 수를 입력해주세요."));
-
-        mockMvc.perform(post(String.format("/api/v1/hello/%d", num))
-                        .with(csrf()))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.resultCode").value("ERROR"))
-                .andExpect(jsonPath("$.result.errorCode").value("BY_ZERO_ERROR"))
-                .andExpect(jsonPath("$.result.message").value("0보다 큰 수를 입력해주세요."))
                 .andDo(print());
     }
 }
