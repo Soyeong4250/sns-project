1799b83281a77dd56aae54c77c174c6cca5ebc7f
이소영 <th42500@naver.com>
2022-12-29 09:32:07 +0900

    [Test] PostEntityFixture - Post 엔티티명 변경으로 인한 코드 수정

--- src/test/java/com/likelion/healing/fixture/PostEntityFixture.java
+++ src/test/java/com/likelion/healing/fixture/PostEntityFixture.java
@@ -1,20 +1,20 @@
 package com.likelion.healing.fixture;
 
-import com.likelion.healing.domain.entity.Post;
+import com.likelion.healing.domain.entity.PostEntity;
 
 import java.time.LocalDateTime;
 
 public class PostEntityFixture {
 
-    public static Post get(String userName, String password) {
-        Post postEntity = Post.builder()
+    public static PostEntity get(String userName, String password) {
+        PostEntity post = PostEntity.builder()
                 .id(1)
                 .user(UserEntityFixture.get(userName, password))
                 .title("title1")
                 .body("body1")
                 .build();
-        postEntity.setCreatedAt(LocalDateTime.now());
-        postEntity.setUpdatedAt(LocalDateTime.now());
-        return postEntity;
+        post.setCreatedAt(LocalDateTime.now());
+        post.setUpdatedAt(LocalDateTime.now());
+        return post;
     }
 }
