c1c04a17235fed589e7e7cb671970aaf338cd8c2
이소영 <th42500@naver.com>
2022-12-29 10:33:51 +0900

    [Fix] HelloController - 자릿수의 합 구하기 API Mapping 수정
    PostMapping 👉 GetMapping

--- src/main/java/com/likelion/healing/controller/HelloController.java
+++ src/main/java/com/likelion/healing/controller/HelloController.java
@@ -5,7 +5,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
@@ -24,7 +27,7 @@ public class HelloController {
     }
 
     @ApiOperation(value = "모든 자릿수의 합 출력해보기", notes = "PostMapping을 이용하여 sumOfDigit 결과를 출력합니다.")
-    @PostMapping("/{num}")
+    @GetMapping("/{num}")
     public Integer sumOfDigit(@PathVariable Integer num) {
         return algorithmService.sumOfDigit(num);
     }
