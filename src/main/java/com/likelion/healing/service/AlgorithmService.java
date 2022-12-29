a4015f1a4e5a2172b6ab5687bce4e74338710685
이소영 <th42500@naver.com>
2022-12-29 10:13:56 +0900

    [Refactor] AlgorithmService  - 잘못된 예외처리 제거

--- src/main/java/com/likelion/healing/service/AlgorithmService.java
+++ src/main/java/com/likelion/healing/service/AlgorithmService.java
@@ -1,16 +1,11 @@
 package com.likelion.healing.service;
 
-import com.likelion.healing.exception.ErrorCode;
-import com.likelion.healing.exception.HealingSnsAppException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AlgorithmService {
 
     public Integer sumOfDigit(Integer num) {
-        if(num <= 0) {
-            throw new HealingSnsAppException(ErrorCode.BY_ZERO_ERROR, "0보다 큰 수를 입력해주세요.");
-        }
 
         Integer sum  = 0;
         while(num > 0) {
