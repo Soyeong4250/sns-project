4eec2034d4988c92c03e3c9822a8cb18d416da62
이소영 <th42500@naver.com>
2023-01-03 11:56:24 +0900

    [Refactor] UserLoginReq - 유효성 검증 Annotation 선언

--- src/main/java/com/likelion/healing/domain/dto/UserLoginReq.java
+++ src/main/java/com/likelion/healing/domain/dto/UserLoginReq.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -13,9 +15,11 @@ import lombok.NoArgsConstructor;
 @Schema(description = "로그인 요청 dto")
 public class UserLoginReq {
 
+    @NotBlank(message = "userName은 필수 값입니다.")
     @Schema(description = "회원 이름")
     private String userName;
 
+    @NotBlank(message = "password는 필수 값입니다.")
     @Schema(description = "회원 비밀번호")
     private String password;
 
