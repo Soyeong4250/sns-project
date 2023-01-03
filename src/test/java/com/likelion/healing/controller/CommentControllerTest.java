a48395499677b75f4fce252584794cb5dd825536
이소영 <th42500@naver.com>
2023-01-04 01:22:19 +0900

    [Test] CommentControllerTest - Comment 목록 조회 성공 Test

--- src/test/java/com/likelion/healing/controller/CommentControllerTest.java
+++ src/test/java/com/likelion/healing/controller/CommentControllerTest.java
@@ -6,24 +6,30 @@ import com.likelion.healing.domain.dto.CommentRes;
 import com.likelion.healing.exception.ErrorCode;
 import com.likelion.healing.exception.HealingSnsAppException;
 import com.likelion.healing.service.CommentService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -42,6 +48,13 @@ class CommentControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    private Integer postId;
+
+    @BeforeEach
+    void setUp() {
+        postId = 3;
+    }
+
     @Nested
     @DisplayName("댓글 등록 테스트")
     class AddCommentTest{
@@ -57,7 +70,7 @@ class CommentControllerTest {
             CommentRes comment = CommentRes.builder()
                                                 .id(1)
                                                 .comment("comment")
-                                                .postId(3)
+                                                .postId(postId)
                                                 .userName("test")
                                                 .createdAt(nowTime)
                                                 .lastModifiedAt(nowTime)
@@ -66,7 +79,7 @@ class CommentControllerTest {
             given(commentService.createComment(any(Integer.class), any(CommentReq.class), any(String.class)))
                     .willReturn(comment);
 
-            mockMvc.perform(post(String.format("/api/v1/posts/%d/comments", 3))
+            mockMvc.perform(post(String.format("/api/v1/posts/%d/comments", postId))
                             .with(csrf())
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsBytes(req)))
@@ -93,7 +106,7 @@ class CommentControllerTest {
             CommentRes comment = CommentRes.builder()
                                                 .id(1)
                                                 .comment("comment")
-                                                .postId(3)
+                                                .postId(postId)
                                                 .userName("test")
                                                 .createdAt(nowTime)
                                                 .lastModifiedAt(nowTime)
@@ -102,7 +115,7 @@ class CommentControllerTest {
             given(commentService.createComment(any(Integer.class), any(CommentReq.class), any(String.class)))
                     .willReturn(comment);
 
-            mockMvc.perform(post(String.format("/api/v1/posts/%d/comments", 3))
+            mockMvc.perform(post(String.format("/api/v1/posts/%d/comments", postId))
                             .with(csrf())
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsBytes(req)))
@@ -119,8 +132,6 @@ class CommentControllerTest {
                     .comment("comment")
                     .build();
 
-            Integer postId = 3;
-
             given(commentService.createComment(any(Integer.class), any(CommentReq.class), any(String.class)))
                     .willThrow(new HealingSnsAppException(ErrorCode.POST_NOT_FOUND, String.format("%d번 게시글은 존재하지 않습니다.", postId)));
 
@@ -136,5 +147,27 @@ class CommentControllerTest {
         }
     }
 
+    @Test
+    @DisplayName("댓글 목록 조회 성공")
+    void getCommentList() throws Exception {
+        List<CommentRes> commentList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            LocalDateTime now = LocalDateTime.now();
+            commentList.add(new CommentRes(i, "comment"+i, "test", postId, now, now));
+        }
+
+
+        given(commentService.getCommentList(any(Integer.class), any(Pageable.class))).willReturn(new PageImpl<>(commentList));
+
+        mockMvc.perform(get(String.format("/api/v1/posts/%d/comments", postId))
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("sort", "createdAt,desc")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("SUCCESS"));
+
+    }
 
 }
