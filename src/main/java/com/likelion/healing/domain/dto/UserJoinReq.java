ecc666f8dfc01696026848374bfcab1e72ca01f5
이소영 <th42500@naver.com>
2023-01-03 11:55:26 +0900

    [Fix] 회원가입 시 UserEntity 대신 UserJoinReq에서 유효성 검증

--- src/main/java/com/likelion/healing/domain/dto/UserJoinReq.java
+++ src/main/java/com/likelion/healing/domain/dto/UserJoinReq.java
@@ -6,6 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor
@@ -14,9 +18,14 @@ import lombok.NoArgsConstructor;
 @Schema(description = "회원가입 요청 dto")
 public class UserJoinReq {
 
+    @NotBlank(message = "userName은 필수 값입니다.")
+    @Length(min = 3, max = 20, message = "userName의 길이를 확인하세요")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$", message = "userName은 한글, 영대소문자, 숫자, -로만 이루어질 수 있습니다")
     @Schema(description = "회원 이름")
     private String userName;
 
+    @NotBlank(message = "password는 필수 값입니다.")
+    @Length(min = 8, max = 50, message = "password의 길이를 확인하세요")
     @Schema(description = "회원 비밀번호")
     private String password;
 
--- src/main/java/com/likelion/healing/domain/entity/UserEntity.java
+++ src/main/java/com/likelion/healing/domain/entity/UserEntity.java
@@ -5,14 +5,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -34,15 +31,10 @@ public class UserEntity extends BaseEntity implements UserDetails {
     private Integer id;
 
     @Column(unique = true)
-    @NotBlank
-    @Length(min = 3, max = 20)
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     @Schema(description = "회원 이름")
     private String userName;
 
     @Column(nullable = false)
-    @NotBlank
-    @Length(min = 8, max = 50)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Schema(description = "회원 비밀번호")
     private String password;
