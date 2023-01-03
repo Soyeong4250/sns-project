1d503471f1642f8940758f31aed88b17d9001059
이소영 <th42500@naver.com>
2023-01-04 01:21:38 +0900

    [Feat] CommentRepository - Comment 목록 조회 추상 메서드 선언

--- src/main/java/com/likelion/healing/repository/CommentRepository.java
+++ src/main/java/com/likelion/healing/repository/CommentRepository.java
@@ -1,7 +1,10 @@
 package com.likelion.healing.repository;
 
 import com.likelion.healing.domain.entity.CommentEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
+    Page<CommentEntity> findAllByPostIdOrderByCreatedAt(Pageable pageable);
 }
