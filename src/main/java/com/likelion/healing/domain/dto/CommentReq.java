2531796b577f21cf047193ff2f29462ab0ebcb3b
이소영 <th42500@naver.com>
2023-01-03 17:44:07 +0900

    [Feat] CommentReq - 댓글 등록, 수정 요청 dto

---
+++ src/main/java/com/likelion/healing/domain/dto/CommentReq.java
@@ -0,0 +1,16 @@
+package com.likelion.healing.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class CommentReq {
+
+    private String comment;
+
+}
