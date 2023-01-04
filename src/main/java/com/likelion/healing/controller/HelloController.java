d18d5618de91d45ae2e6fb0c765a0b14e07788df
이소영 <th42500@naver.com>
2023-01-04 17:36:42 +0900

    [Refactor] HelloController - Swagger에 뜨지 않도록 설정

--- src/main/java/com/likelion/healing/controller/HelloController.java
+++ src/main/java/com/likelion/healing/controller/HelloController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @Api(tags = {"00. HelloController"})
 @RequestMapping("/api/v1/hello")
-public class HelloController {
+public @ApiIgnore class HelloController {
 
     private final AlgorithmService algorithmService;
 
