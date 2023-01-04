8480da1e86949b858719f405ca06567cd1b2de9f
이소영 <th42500@naver.com>
2023-01-04 17:38:58 +0900

    [Feat] Comment 응답 dto Swagger 적용

--- src/main/java/com/likelion/healing/domain/dto/CommentDeleteRes.java
+++ src/main/java/com/likelion/healing/domain/dto/CommentDeleteRes.java
@@ -1,5 +1,6 @@
 package com.likelion.healing.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +8,9 @@ import lombok.Getter;
 @Getter
 public class CommentDeleteRes {
 
+    @Schema(description = "로직 수행 결과")
     private String message;
+    @Schema(description = "댓글 번호")
     private Integer id;
 
 }
--- src/main/java/com/likelion/healing/domain/dto/CommentRes.java
+++ src/main/java/com/likelion/healing/domain/dto/CommentRes.java
@@ -1,6 +1,7 @@
 package com.likelion.healing.domain.dto;
 
 import com.likelion.healing.domain.entity.CommentEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +15,22 @@ import java.time.LocalDateTime;
 @Builder
 public class CommentRes {
 
+    @Schema(description = "댓글 번호")
     private Integer id;
+
+    @Schema(description = "댓글 내용")
     private String comment;
+
+    @Schema(description = "작성자")
     private String userName;
+
+    @Schema(description = "포스트 번호")
     private Integer postId;
+
+    @Schema(description = "생성 시간")
     private LocalDateTime createdAt;
+
+    @Schema(description = "마지막 수정 시간")
     private LocalDateTime lastModifiedAt;
 
     public static CommentRes of(CommentEntity commentEntity) {
