44c2532365e86315e8e3c744e7a9b2fa356708e0
이소영 <th42500@naver.com>
2023-01-04 10:56:28 +0900

    [Feat] ErrorCode - Comment가 없는 경우 코드 추가

--- src/main/java/com/likelion/healing/exception/ErrorCode.java
+++ src/main/java/com/likelion/healing/exception/ErrorCode.java
@@ -15,8 +15,8 @@ public enum ErrorCode {
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
     INVALID_USERROLE(HttpStatus.FORBIDDEN, "사용자가 권한이 없습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러")
-    ;
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글이 없습니다.");
 
     private HttpStatus status;
     private String message;
