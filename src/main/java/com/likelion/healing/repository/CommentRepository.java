8bc0bc1f4331903da2b9f49861bf84e473007bc5
이소영 <th42500@naver.com>
2023-01-04 15:35:29 +0900

    [Feat] CommentRepository - findByPostIdAndId() 추가

--- src/main/java/com/likelion/healing/repository/CommentRepository.java
+++ src/main/java/com/likelion/healing/repository/CommentRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
     Page<CommentEntity> findByPostId(Integer postId, Pageable pageable);
+
+    Optional<CommentEntity> findByPostIdAndId(Integer postId, Integer commentId);
 }
