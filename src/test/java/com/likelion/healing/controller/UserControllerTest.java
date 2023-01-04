1a59a238889cda4e2d441bf336b14899b4122485
이소영 <th42500@naver.com>
2023-01-04 16:59:16 +0900

    [Test] UserControllerTest - 테스트코드 가독성을 위한 @Nested 선언

--- src/test/java/com/likelion/healing/controller/UserControllerTest.java
+++ src/test/java/com/likelion/healing/controller/UserControllerTest.java
@@ -9,6 +9,7 @@ import com.likelion.healing.exception.ErrorCode;
 import com.likelion.healing.exception.HealingSnsAppException;
 import com.likelion.healing.service.UserService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -37,107 +38,117 @@ class UserControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Test
-    @WithMockUser
-    @DisplayName("회원가입 성공")
-    void successfulJoin() throws Exception {
-        UserJoinReq req = UserJoinReq.builder()
-                .userName("Soyeong")
-                .password("12345")
-                .build();
+    @Nested
+    @DisplayName("회원가입 테스트")
+    public class JoinTest {
 
-        given(userService.join(any(UserJoinReq.class))).willReturn(new UserJoinRes(1, "Soyeong"));
+        @Test
+        @WithMockUser
+        @DisplayName("회원가입 성공")
+        void successfulJoin() throws Exception {
+            UserJoinReq req = UserJoinReq.builder()
+                    .userName("Soyeong")
+                    .password("12345678")
+                    .build();
 
-        mockMvc.perform(post("/api/v1/users/join")
+            given(userService.join(any(UserJoinReq.class))).willReturn(new UserJoinRes(1, "Soyeong"));
+
+            mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(req)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+                    .andExpect(jsonPath("$.result.userId").value(1))
+                    .andExpect(jsonPath("$.result.userName").value("Soyeong"))
+                    .andDo(print());
+        }
+
+        @Test
+        @WithMockUser
+        @DisplayName("회원가입 실패 - userName 중복인 경우")
+        void failedJoin() throws Exception {
+            UserJoinReq req = UserJoinReq.builder()
+                    .userName("Soyeong")
+                    .password("12345678")
+                    .build();
+
+            given(userService.join(any(UserJoinReq.class))).willThrow(new HealingSnsAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s은(는) 이미 있습니다.", req.getUserName())));
+
+            mockMvc.perform(post("/api/v1/users/join")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(req)))
+                    .andDo(print())
+                    .andExpect(status().isConflict());
+        }
+    }
+
+    @Nested
+    @DisplayName("로그인 테스트")
+    public class LoginTest {
+
+        @Test
+        @WithMockUser
+        @DisplayName("로그인 성공")
+        void successfulLogin() throws Exception {
+            UserLoginReq req = UserLoginReq.builder()
+                    .userName("Soyeong")
+                    .password("12345678")
+                    .build();
+
+            given(userService.login(any(UserLoginReq.class))).willReturn(new UserLoginRes("token"));
+
+            mockMvc.perform(post("/api/v1/users/login")
                     .with(csrf())
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(objectMapper.writeValueAsBytes(req)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
-                .andExpect(jsonPath("$.result.userId").value(1))
-                .andExpect(jsonPath("$.result.userName").value("Soyeong"))
-                .andDo(print());
-    }
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.result.jwt").exists())
+                    .andDo(print());
+        }
 
-    @Test
-    @WithMockUser
-    @DisplayName("회원가입 실패 - userName 중복인 경우")
-    void failedJoin() throws Exception {
-        UserJoinReq req = UserJoinReq.builder()
-                .userName("Soyeong")
-                .password("12345")
-                .build();
+        @Test
+        @WithMockUser
+        @DisplayName("로그인 실패 - 존재하지 않는 회원")
+        void notFoundUser() throws Exception {
+            UserLoginReq req = UserLoginReq.builder()
+                    .userName("Soyeong")
+                    .password("12345678")
+                    .build();
 
-        given(userService.join(any(UserJoinReq.class))).willThrow(new HealingSnsAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s은(는) 이미 있습니다.", req.getUserName())));
+            given(userService.login(any(UserLoginReq.class))).willThrow(new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 없는 회원입니다.", req.getUserName())));
 
-        mockMvc.perform(post("/api/v1/users/join")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(req)))
-                .andDo(print())
-                .andExpect(status().isConflict());
-    }
+            mockMvc.perform(post("/api/v1/users/login")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(req)))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.result.errorCode").value("USERNAME_NOT_FOUND"))
+                    .andExpect(jsonPath("$.result.message").value(req.getUserName() + "은(는) 없는 회원입니다."))
+                    .andDo(print());
+        }
 
-    @Test
-    @WithMockUser
-    @DisplayName("로그인 성공")
-    void successfulLogin() throws Exception {
-        UserLoginReq req = UserLoginReq.builder()
-                .userName("Soyeong")
-                .password("12345")
-                .build();
+        @Test
+        @WithMockUser
+        @DisplayName("로그인 실패 - 비밀번호가 일치하지 않는 경우")
+        void invalidPassword() throws Exception {
+            UserLoginReq req = UserLoginReq.builder()
+                    .userName("Soyeong")
+                    .password("12345678")
+                    .build();
 
-        given(userService.login(any(UserLoginReq.class))).willReturn(new UserLoginRes("token"));
+            given(userService.login(any(UserLoginReq.class))).willThrow(new HealingSnsAppException(ErrorCode.INVALID_PASSWORD, "회원 이름 또는 비밀번호를 다시 확인해주세요."));
 
-        mockMvc.perform(post("/api/v1/users/login")
-                .with(csrf())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(req)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.result.jwt").exists())
-                .andDo(print());
-    }
-
-    @Test
-    @WithMockUser
-    @DisplayName("로그인 실패 - 존재하지 않는 회원")
-    void notFoundUser() throws Exception {
-        UserLoginReq req = UserLoginReq.builder()
-                .userName("Soyeong")
-                .password("12345")
-                .build();
-
-        given(userService.login(any(UserLoginReq.class))).willThrow(new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 없는 회원입니다.", req.getUserName())));
-
-        mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(req)))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.result.errorCode").value("USERNAME_NOT_FOUND"))
-                .andExpect(jsonPath("$.result.message").value(req.getUserName() + "은(는) 없는 회원입니다."))
-                .andDo(print());
-    }
-
-    @Test
-    @WithMockUser
-    @DisplayName("로그인 실패 - 비밀번호가 일치하지 않는 경우")
-    void invalidPassword() throws Exception {
-        UserLoginReq req = UserLoginReq.builder()
-                .userName("Soyeong")
-                .password("12345")
-                .build();
-
-        given(userService.login(any(UserLoginReq.class))).willThrow(new HealingSnsAppException(ErrorCode.INVALID_PASSWORD, "회원 이름 또는 비밀번호를 다시 확인해주세요."));
-
-        mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(req)))
-                .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.result.errorCode").value("INVALID_PASSWORD"))
-                .andExpect(jsonPath("$.result.message").value("회원 이름 또는 비밀번호를 다시 확인해주세요."))
-                .andDo(print());
+            mockMvc.perform(post("/api/v1/users/login")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(req)))
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.result.errorCode").value("INVALID_PASSWORD"))
+                    .andExpect(jsonPath("$.result.message").value("회원 이름 또는 비밀번호를 다시 확인해주세요."))
+                    .andDo(print());
+        }
     }
 
     /*@Test
