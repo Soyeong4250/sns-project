44fda92fd1ca3e656188910ad760529abd34f787
이소영 <th42500@naver.com>
2023-01-04 16:08:44 +0900

    [Feat] CommentDeleteRes -  Comment 삭제 응답 dto

---
+++ src/main/java/com/likelion/healing/domain/dto/CommentDeleteRes.java
@@ -0,0 +1,13 @@
+package com.likelion.healing.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CommentDeleteRes {
+
+    private String message;
+    private Integer id;
+
+}
