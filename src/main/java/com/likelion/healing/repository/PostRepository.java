088a8e01f3678c61331714b9788266d4fd32d247
이소영 <th42500@naver.com>
2023-01-05 16:50:41 +0900

    [Feat] PostRepository - 마이피드 조회를 위한 메서드 추가

--- src/main/java/com/likelion/healing/repository/PostRepository.java
+++ src/main/java/com/likelion/healing/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.likelion.healing.repository;
 
 import com.likelion.healing.domain.entity.PostEntity;
+import com.likelion.healing.domain.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
     Page<PostEntity> findAll(Pageable pageable);
 
+    Page<PostEntity> findByUser(UserEntity user, Pageable pageable);
 }
