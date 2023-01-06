dc6ec3ef74a7f3bdd0d293c42f2a30cc1f2054c3
이소영 <th42500@naver.com>
2023-01-06 18:10:47 +0900

    [Feat] LikeEntity - LikeEntity 추가

---
+++ src/main/java/com/likelion/healing/domain/entity/LikeEntity.java
@@ -0,0 +1,33 @@
+package com.likelion.healing.domain.entity;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "likes")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class LikeEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PostEntity post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private UserEntity user;
+
+    @Builder
+    public LikeEntity(Integer id, PostEntity post, UserEntity user) {
+        this.id = id;
+        this.post = post;
+        this.user = user;
+    }
+}
