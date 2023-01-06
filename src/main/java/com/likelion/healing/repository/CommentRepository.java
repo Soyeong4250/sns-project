23892f49af90c2717bd5be418a3c0e4dbcf1f40e
이소영 <th42500@naver.com>
2023-01-06 18:19:45 +0900

    [Feat] Repository에 @Repository 선언

--- src/main/java/com/likelion/healing/repository/CommentRepository.java
+++ src/main/java/com/likelion/healing/repository/CommentRepository.java
@@ -4,9 +4,11 @@ import com.likelion.healing.domain.entity.CommentEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
     Page<CommentEntity> findByPostId(Integer postId, Pageable pageable);
 
--- src/main/java/com/likelion/healing/repository/PostRepository.java
+++ src/main/java/com/likelion/healing/repository/PostRepository.java
@@ -5,7 +5,9 @@ import com.likelion.healing.domain.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
     Page<PostEntity> findAll(Pageable pageable);
 
