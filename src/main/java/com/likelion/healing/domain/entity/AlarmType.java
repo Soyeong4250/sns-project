c96d336f5c1c5c916a7b55b26fa298a98a75c35a
이소영 <th42500@naver.com>
2023-01-08 02:11:57 +0900

    [Refactor] AlarmType - 댓글이 달렸을 때의 메세지 오타 수정

--- src/main/java/com/likelion/healing/domain/entity/AlarmType.java
+++ src/main/java/com/likelion/healing/domain/entity/AlarmType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum AlarmType {
 
-    NEW_COMMENT_ON_POST("new cmment!"),
+    NEW_COMMENT_ON_POST("new comment!"),
     NEW_LIKE_ON_POST("new like!")
     ;
 
