b228bfa4179af03a091da0717d56810d5213cbdf
이소영 <th42500@naver.com>
2023-01-08 00:34:39 +0900

    [Refactor] AlarmRepository - @Repository 선언

--- src/main/java/com/likelion/healing/repository/AlarmRepository.java
+++ src/main/java/com/likelion/healing/repository/AlarmRepository.java
@@ -5,7 +5,9 @@ import com.likelion.healing.domain.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlarmRepository extends JpaRepository<AlarmEntity, Integer> {
 
     Page<AlarmEntity> findByUser(UserEntity user, Pageable pageable);
