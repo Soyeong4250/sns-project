1455e73b1a23fde95869dc566308a2f76c0852e4
이소영 <th42500@naver.com>
2023-01-08 00:32:30 +0900

    [Refactor] CommentReq - 댓글 유효성 검증을 위한 @NotBlank 선언

--- src/main/java/com/likelion/healing/domain/dto/CommentReq.java
+++ src/main/java/com/likelion/healing/domain/dto/CommentReq.java
@@ -7,12 +7,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 public class CommentReq {
 
+    @NotBlank(message = "댓글 내용이 비어있습니다.")
     @Schema(description = "댓글 내용")
     private String comment;
 
