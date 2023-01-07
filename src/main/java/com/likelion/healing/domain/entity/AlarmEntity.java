9a28ca364b29ff8a318e4badd608c8e2b5cb5841
이소영 <th42500@naver.com>
2023-01-07 20:48:20 +0900

    [Feat] AlarmEntity - 알람 엔티티

---
+++ src/main/java/com/likelion/healing/domain/entity/AlarmEntity.java
@@ -0,0 +1,39 @@
+package com.likelion.healing.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class AlarmEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private AlarmType alarmType;
+
+    @Column(nullable = false)
+    private Integer fromUserId;
+
+    @Column(nullable = false)
+    private Integer targetId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Builder
+    public AlarmEntity(Integer id, AlarmType alarmType, Integer fromUserId, Integer targetId, UserEntity user) {
+        this.id = id;
+        this.alarmType = alarmType;
+        this.fromUserId = fromUserId;
+        this.targetId = targetId;
+        this.user = user;
+    }
+}
