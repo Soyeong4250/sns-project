72f8960b2a3e20cebdcefe840d91dc5667639aa0
이소영 <th42500@naver.com>
2023-01-07 23:08:51 +0900

    [Fix] AlarmRepository - User를 이용한 알람 목록 조회 시 Entity를 반환하도록 수정

--- src/main/java/com/likelion/healing/repository/AlarmRepository.java
+++ src/main/java/com/likelion/healing/repository/AlarmRepository.java
@@ -1,6 +1,5 @@
 package com.likelion.healing.repository;
 
-import com.likelion.healing.domain.dto.AlarmRes;
 import com.likelion.healing.domain.entity.AlarmEntity;
 import com.likelion.healing.domain.entity.UserEntity;
 import org.springframework.data.domain.Page;
@@ -9,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlarmRepository extends JpaRepository<AlarmEntity, Integer> {
 
-    Page<AlarmRes> findByUser(UserEntity user, Pageable pageable);
+    Page<AlarmEntity> findByUser(UserEntity user, Pageable pageable);
 
 }
