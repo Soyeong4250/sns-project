4703bc32459730f0f925195209a736ed853a7551
이소영 <th42500@naver.com>
2023-01-10 14:00:43 +0900

    [Refactor] SecurityConfig - SpringSecurity 접근권한 재설정 및 SecurityException 적용

--- src/main/java/com/likelion/healing/config/SecurityConfig.java
+++ src/main/java/com/likelion/healing/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.likelion.healing.config;
 
 import com.likelion.healing.service.UserService;
+import com.likelion.healing.util.JwtExceptionFilter;
+import com.likelion.healing.util.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -31,15 +31,21 @@ public class SecurityConfig {
                 .cors().and()
                 .authorizeRequests()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login", "/api/v1/hello/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
-                .antMatchers("**exception**").permitAll()
-//                .antMatchers("/api/v1/users/{userId}/role/change").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/v1/posts/my", "/api/v1/alarms").authenticated()
+                .antMatchers("/api/v1/users/{userId}/role/change").hasAnyRole("ROLE_ADMIN", "ROLE_MASTER")
                 .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(), JwtTokenFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())  // 인증 실패
+                .accessDeniedHandler(new CustomAccessDeniedHandler())  // 인가 실패
+                .and()
                 .build();
     }
 }
