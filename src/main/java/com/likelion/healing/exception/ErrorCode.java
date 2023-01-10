de2d41dde92e0c3b7761be4b6a2aeb90c7fb07bc
이소영 <th42500@naver.com>
2023-01-10 17:19:52 +0900

    [Refactor] ErrorCode - 사용하지 않는 에러코드 제거

--- src/main/java/com/likelion/healing/exception/ErrorCode.java
+++ src/main/java/com/likelion/healing/exception/ErrorCode.java
@@ -16,9 +16,7 @@ public enum ErrorCode {
     FORBIDDEN_USERROLE(HttpStatus.FORBIDDEN, "사용자가 권한이 없습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 에러"),
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글이 없습니다."),
-    DUPLICATED_LIKE(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다."),
-    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 누른 적이 없습니다.");
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글이 없습니다.");
 
     private HttpStatus status;
     private String message;
