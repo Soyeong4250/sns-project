2ee759927646db3370b2a6657255d09c247ea569
이소영 <th42500@naver.com>
2023-01-10 16:38:25 +0900

    [Fix] BaseEntity - @prePersist(), @preUpdate() + ZoneDateTime 적용

--- src/main/java/com/likelion/healing/domain/entity/BaseEntity.java
+++ src/main/java/com/likelion/healing/domain/entity/BaseEntity.java
@@ -7,12 +7,10 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Getter
 @Setter
@@ -22,17 +20,28 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(updatable = false)
     @Schema(description = "생성시간", example = "yyyy-mm-dd hh:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Schema(description = "마지막 수정시간", example = "yyyy-mm-dd hh:mm:ss")
     private LocalDateTime updatedAt;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Schema(description = "삭제시간", example = "yyyy-mm-dd hh:mm:ss")
     private LocalDateTime deletedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now(ZoneId.of("KST"));
+        this.updatedAt = LocalDateTime.now(ZoneId.of("KST"));
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now(ZoneId.of("KST"));
+    }
 }
