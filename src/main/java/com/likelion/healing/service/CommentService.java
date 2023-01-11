7106a4b952d44e7d55d689510726e45afe99d2fe
이소영 <th42500@naver.com>
2023-01-11 16:38:40 +0900

    [Refactor] CommentService - 중복 로직 외부 메서드로 분리

--- src/main/java/com/likelion/healing/service/CommentService.java
+++ src/main/java/com/likelion/healing/service/CommentService.java
@@ -31,11 +31,10 @@ public class CommentService {
     @Transactional
     public CommentRes createComment(Integer postId, @Valid CommentReq commentReq, String userName) {
 
-        PostEntity post = postRepository.findById(postId)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.POST_NOT_FOUND, String.format("%d번 포스트가 존재하지 않습니다.", postId)));
-
+        PostEntity post = findPostById(postId);
         UserEntity user = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 존재하지 않는 회원입니다", userName)));
+
         CommentEntity commentEntity = commentReq.toEntity();
         commentEntity.setUser(user);
         commentEntity.setPost(post);
@@ -50,8 +49,7 @@ public class CommentService {
 
     @Transactional(readOnly = true)
     public Page<CommentRes> getCommentList(Integer postId, Pageable pageable) {
-       postRepository.findById(postId)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.POST_NOT_FOUND, String.format("%d번 포스트가 존재하지 않습니다.", postId)));
+        findPostById(postId);
 
         Page<CommentRes> commentList = commentRepository.findByPostId(postId, pageable).map(CommentRes::of);
         return commentList;
@@ -59,14 +57,9 @@ public class CommentService {
 
     @Transactional
     public CommentRes updateComment(Integer postId, Integer commentId, @Valid CommentReq commentReq, String userName) {
-        postRepository.findById(postId)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.POST_NOT_FOUND, String.format("%d번 포스트가 존재하지 않습니다.", postId)));
-
-        CommentEntity comment = commentRepository.findByPostIdAndId(postId, commentId)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.COMMENT_NOT_FOUND, String.format("%d번 포스트에는 %d번 댓글이 존재하지 않습니다.", postId, commentId)));
-
-        UserEntity user = userRepository.findByUserName(userName)
-                                        .orElseThrow(() -> new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 존재하지 않는 회원입니다.", userName)));
+        findPostById(postId);
+        CommentEntity comment = findCommentByPostAndId(postId, commentId);
+        UserEntity user = findUserByUserName(userName);
 
         if(!user.getRole().equals(UserRole.ADMIN.getAuthority()) && !user.getUserName().equals(comment.getUser().getUserName())) {
             throw new HealingSnsAppException(ErrorCode.INVALID_PERMISSION, "사용자가 권한이 없습니다.");
@@ -83,15 +76,9 @@ public class CommentService {
 
     @Transactional
     public CommentDeleteRes deleteComment(Integer postId, Integer commentId, String userName) {
-        postRepository.findById(postId)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.POST_NOT_FOUND, String.format("%d번 포스트가 존재하지 않습니다.", postId)));
-
-        UserEntity user = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 존재하지 않는 회원입니다.", userName)));
-
-
-        CommentEntity comment = commentRepository.findByPostIdAndId(postId, commentId)
-                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.COMMENT_NOT_FOUND, String.format("%d번 포스트에는 %d번 댓글이 존재하지 않습니다.", postId, commentId)));
+        findPostById(postId);
+        UserEntity user = findUserByUserName(userName);
+        CommentEntity comment = findCommentByPostAndId(postId, commentId);
 
         if(!user.getRole().equals(UserRole.ADMIN.getAuthority()) && !user.getUserName().equals(comment.getUser().getUserName())) {
             throw new HealingSnsAppException(ErrorCode.INVALID_PERMISSION, "사용자가 권한이 없습니다.");
@@ -99,4 +86,21 @@ public class CommentService {
         commentRepository.deleteById(commentId);
         return new CommentDeleteRes("댓글 삭제 완료", commentId);
     }
+
+    private UserEntity findUserByUserName(String userName) {
+        return userRepository.findByUserName(userName)
+                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.USERNAME_NOT_FOUND, String.format("%s은(는) 존재하지 않는 회원입니다.", userName)));
+    }
+
+    private PostEntity findPostById(Integer postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.POST_NOT_FOUND, String.format("%d번 포스트가 존재하지 않습니다.", postId)));
+    }
+
+    private CommentEntity findCommentByPostAndId(Integer postId, Integer commentId) {
+        return commentRepository.findByPostIdAndId(postId, commentId)
+                .orElseThrow(() -> new HealingSnsAppException(ErrorCode.COMMENT_NOT_FOUND, String.format("%d번 포스트에는 %d번 댓글이 존재하지 않습니다.", postId, commentId)));
+    }
+
+
 }
